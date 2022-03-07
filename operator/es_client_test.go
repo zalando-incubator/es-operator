c7c6282c0efd4a1925fdfd70b839576b8af5c5a5
Girish Chandrashekar <girish.chandrashekar@zalando.de>
2022-03-07 10:03:49 +0100

    Fix failing tests and regenerate files
    Signed-off-by: Girish Chandrashekar <girish.chandrashekar@zalando.de>

--- operator/es_client_test.go
+++ operator/es_client_test.go
@@ -42,7 +42,7 @@ func TestDrain(t *testing.T) {
 	info := httpmock.GetCallCountInfo()
 	require.EqualValues(t, 1, info["GET http://elasticsearch:9200/_cluster/health"])
 	require.EqualValues(t, 2, info["PUT http://elasticsearch:9200/_cluster/settings"])
-	require.EqualValues(t, 1, info["GET http://elasticsearch:9200/_cluster/settings"])
+	require.EqualValues(t, 2, info["GET http://elasticsearch:9200/_cluster/settings"])
 	require.EqualValues(t, 1, info["GET http://elasticsearch:9200/_cat/shards"])
 }
 
